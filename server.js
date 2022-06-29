7788c8903f9e21444b7a86e00678701b1d1f4dae
kokoc <kokoc@kokoc.cc>
2022-06-29 20:01:40 +0300

    readme; server

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const { PORT = 3000 } = process.env;
 const staticPath = path.join(__dirname, 'dist');
 
 app.use(express.static(staticPath));
 
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+  console.log(`Listening on: http://localhost:${PORT}`);
 });
