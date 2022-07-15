3c51864712cbbdfc9598eec7f821066329759aca
kokoc <kokoc@kokoc.cc>
2022-07-15 04:50:22 +0300

    package, server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const express = require('express');
 
@@ -6,6 +7,11 @@ const { PORT = 3000 } = process.env;
 const staticPath = path.join(__dirname, 'dist');
 
 app.use(express.static(staticPath));
+
+app.get('*', (_, res) => {
+  const indexPath = path.join(__dirname, 'dist', 'index.html');
+  res.sendFile(indexPath);
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
